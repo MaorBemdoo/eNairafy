cd2fc763be1ec610d9b22f1678c7ecdf900d4e03
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-03 18:28:47 +0100

    add bg url instead of using Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import Image from "next/image";
-import heroImg from "../../public/hero.jpg"
 import Button from "@/components/Button";
 import Link from "next/link";
 
@@ -9,15 +7,14 @@ export default function Home() {
   return (
     <main>
       <section className="w-full" id="hero">
-        <div className="relative">
-          <div className="absolute bottom-7 w-full">
-            <div className="container mx-auto px-4">
+        <div className="bg-[url(../../public/hero.jpg)] bg-cover bg-fixed h-[500px] w-full">
+          <div className="container mx-auto p-4 flex items-end h-full">
+            <div className="max-w-[50%] md:max-w-max">
               <h1 className="text-6xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-transparent to-green-600">Elevate Your<br/>Tech Experience</h1>
               <p className="text-lg mb-2">Explore the latest in cutting-edge technology<br/>and elevate your digital lifestyle.</p>
               <Link href="/products"><Button color="black">Shop Now</Button></Link>
             </div>
           </div>
-          <Image src={heroImg} alt="hero img" className="w-full h-[500px] object-cover" priority/>
         </div>
         <div>
           {/* Companies sponsors like samsung */}
