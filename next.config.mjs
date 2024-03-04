ceecd1ac58e0dca629b9402164da2dcaf29a2b20
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-04 19:49:20 +0100

    Add space

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+import path from "path"
+
 const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    sassOptions: {
+        includePaths: [path.join(process.cwd(), "/src/styles")]
+    }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <div className="container mx-auto p-4 flex items-end h-full">
             <div className="max-w-[50%] md:max-w-max">
               <h1 className="text-6xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-transparent to-green-600">Elevate Your<br/>Tech Experience</h1>
-              <p className="text-lg mb-2">Explore the latest in cutting-edge technology<br/>and elevate your digital lifestyle.</p>
+              <p className="text-lg my-5">Explore the latest in cutting-edge technology<br/>and elevate your digital lifestyle.</p>
               <Link href="/products"><Button color="black">Shop Now</Button></Link>
             </div>
           </div>
