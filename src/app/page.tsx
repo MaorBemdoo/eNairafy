220647ee7e294442ee06b0772c39c0f95a76fd9b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-05 03:47:29 +0100

    add word break at mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="w-full" id="hero">
         <div className="bg-[url(/hero.jpg)] bg-cover bg-center bg-fixed h-[500px] w-full">
           <div className="container mx-auto p-4 flex items-end h-full">
-            <div className="max-w-[50%] md:max-w-max">
+            <div className="max-w-[50%] md:max-w-max mobile:break-all">
               <h1 className="text-6xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-transparent to-green-600">Elevate Your<br/>Tech Experience</h1>
               <p className="text-lg my-5">Explore the latest in cutting-edge technology<br/>and elevate your digital lifestyle.</p>
               <Link href="/products"><Button color="black">Shop Now</Button></Link>
