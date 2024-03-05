0680913e5906b00eec3b0004c3c96cd7ad4e4247
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-05 09:48:39 +0100

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
       <section className="w-full" id="hero">
-        <div className="bg-[url(/hero.jpg)] bg-cover bg-center bg-fixed h-[500px] w-full">
+        <div className="bg-[url(/hero.jpg)] bg-cover bg-center bg-fixed h-[500px] w-full mobile:min-h-[90dvh]">
           <div className="container mx-auto p-4 flex items-end h-full">
             <div className="max-w-[50%] md:max-w-max mobile:break-all">
               <h1 className="text-6xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-transparent to-green-600">Elevate Your<br/>Tech Experience</h1>
