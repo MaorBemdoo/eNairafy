ce7a4393bed5e3ea241ef2453a16f1ac22af5c31
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-12 21:46:06 +0100

    Change from Date api to moment

--- src/app/api/reset-timer/route.ts
+++ src/app/api/reset-timer/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
+import moment from "moment-timezone"
 
 export async function GET(req: NextRequest){
     const authHeader = req.headers.get('Authorization');
@@ -8,11 +9,19 @@ export async function GET(req: NextRequest){
         });
     }
     try {
+        const now = moment();
+        const midnight = moment().tz('Africa/Lagos').startOf('day');  // Set to midnight in your time zone
+        let timeUntilMidnight = midnight.diff(now);
+        if (timeUntilMidnight < 0) {
+            midnight.add(1, 'days');
+            timeUntilMidnight = midnight.diff(now);
+        }
+
         // Calculate time until next midnight
-        const now = new Date();
-        const midnight = new Date(now)
-        midnight.setHours(24, 0, 0, 0); // Set to midnight
-        const timeUntilMidnight = midnight.getTime() - now.getTime();
+        // const now = new Date();
+        // const midnight = new Date(now)
+        // midnight.setHours(24, 0, 0, 0); // Set to midnight
+        // const timeUntilMidnight = midnight.getTime() - now.getTime();
 
         // Send the time until midnight as the new countdown value
         return NextResponse.json({ countdown: timeUntilMidnight }, {
