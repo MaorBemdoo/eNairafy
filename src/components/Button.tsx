3766724bb1594f7bc798dc42f7f6f9e2460cb7dc
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-13 00:15:09 +0100

    improve button ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,17 @@
-import { ButtonProps } from "@/types"
+import { ButtonProps } from "@/types";
 
-const Button = ({ children, color="green", onClick, className }: ButtonProps) => {
+const Button = ({ children, color = "green", onClick, className}: ButtonProps) => {
     return (
-        <button className={`p-3 rounded-md text-white ${color == "green" ? "bg-green-600 hover:bg-slate-950" : "bg-slate-950 hover:bg-green-600"} btn ${className}`} onClick={onClick}>{children}</button>
-    )
-}
-export default Button
+        <button
+            className={`p-3 rounded-md text-white dark:text-black border-2 border-slate-950 dark:border-slate-50 ${
+                color == "green"
+                    ? "bg-green-600 hover:bg-slate-950 dark:hover:bg-slate-50"
+                    : "bg-slate-950 dark:bg-slate-50 hover:bg-green-600"
+            } btn ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
+export default Button;
