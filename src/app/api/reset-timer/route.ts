1bf3dceb0947d86128aeb2f7339a42caa7097eaf
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-12 01:11:18 +0100

    use revalidate to stop cache

--- src/app/api/reset-timer/route.ts
+++ src/app/api/reset-timer/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
-export const fetchCache = "no-store"
+export const fetchCache = "force-no-store"
 
 import { NextResponse, type NextRequest } from "next/server";
 
