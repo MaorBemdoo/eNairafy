98a2aa785356456afd59c613cb29519003c5759a
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-30 16:26:52 +0100

    remove default cache, so that update to discount.txt will work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
 import HomeDiscount from "@/components/HomeDiscount";
 
+export const revalidate = 0;
+
 export default async function Home() {
     return (
         <main>
