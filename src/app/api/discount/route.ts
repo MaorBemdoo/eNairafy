906f3978b70635e26fc2ac19aeabb94d96974ce3
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-30 14:00:20 +0100

    fix

--- src/app/api/discount/route.ts
+++ src/app/api/discount/route.ts
@@ -12,8 +12,7 @@ export async function GET(req: NextRequest){
     }
     
     try {
-        // const newDiscount = await createDiscount()
-        const newDiscount = "Testing"
+        const newDiscount = await createDiscount()
         await writeFile(`public/discount.txt`, `${JSON.stringify(newDiscount)}`).then(res => res).catch(err => console.log(err))
         return NextResponse.json({ message: `Created new discount` }, {status: 200})
     } catch (err) {
--- src/utils/getDiscountProducts.ts
+++ src/utils/getDiscountProducts.ts
@@ -3,11 +3,11 @@ import { getProducts } from "./actions/getProducts";
 
 export async function getDiscountProducts() {
     try {
-        // const products = await getProducts({limit: 200})
+        const products = await getProducts({limit: 200})
         const discountProductsId = await getDiscountProductsId("fetch");
 
-        // const discountProducts = products.data.filter((product: any) => discountProductsId.includes(product.id))
-        // return discountProducts
+        const discountProducts = products.data.filter((product: any) => discountProductsId.includes(product.id))
+        return discountProducts
     } catch (err) {
         console.log(err);
         return err;
