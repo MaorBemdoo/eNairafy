da10916003ac0987cb8c3f4faf7077e9c7dcc042
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-04-04 23:18:05 +0100

    fix

--- src/utils/actions/getDiscount.ts
+++ src/utils/actions/getDiscount.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export async function getDiscount(){
-    const res = await axios.get("https://api.chec.io/v1/discount", {
+    const res = await axios.get("https://api.chec.io/v1/discounts", {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
