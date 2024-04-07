6826e9bb950ea0e984194ec51e7dde9287b7035c
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-04-07 21:18:15 +0100

    fix

--- src/components/HomeDiscount/DiscountProducts.tsx
+++ src/components/HomeDiscount/DiscountProducts.tsx
@@ -3,12 +3,10 @@ import { getDiscountProducts } from "@/utils/getDiscountProducts"
 const DiscountProducts = async () => {
 
     const discountProducts = await getDiscountProducts()
-    console.log(discountProducts)
 
     return (
         <div>
             {
-                // JSON.stringify(discountProducts)
                 // discountProducts.map(({}) => {
                 //     return (
                 //         <div>
